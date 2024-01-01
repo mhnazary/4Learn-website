9272567933d23b3d9e9db9981511ce8a97c14914
mhnazary <mhnazary2020@gmail.com>
2024-01-02 00:52:09 +0430

    Added bar icon from font awesome package.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
   return (
@@ -7,8 +9,8 @@ function Navbar() {
         <h1 className="text-3xl font-bold font-poppins text-green">
           4Learn-App
         </h1>
-        <div className="navbar w-9/12 flex justify-between text-center">
-          <div className="nav flex justify-evenly w-10/12 text-center text-base pt-2 text-navbar font-sans">
+        <div className=" lg:w-9/12 flex justify-between text-center">
+          <div className="nav hidden lg:flex justify-evenly w-10/12 text-center text-base pt-2 text-navbar font-sans">
             <NavLink to="/">Home</NavLink>
             <NavLink to="/About">About</NavLink>
             <NavLink to="/Courses">Courses</NavLink>
@@ -18,10 +20,15 @@ function Navbar() {
             <NavLink to="/Dropdown">Dropdown</NavLink>
             <NavLink to="/Contacts">Contacts</NavLink>
           </div>
-          <button className="start bg-green text-white px-5 rounded-3xl font-sans">
-            {" "}
-            Get started
-          </button>
+          <div className='flex justify-between items-center '>
+            <button className="start bg-green text-white px-5 py-2 rounded-3xl font-sans">
+              {" "}
+              Get started
+            </button>
+            <div className='ml-5 cursor-pointer text-2xl lg:hidden'>
+              <FontAwesomeIcon icon={faBars} />
+            </div>
+          </div>
         </div>
       </header>
     </>

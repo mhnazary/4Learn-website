dcac461430520a85b3f0c14f63719d9eab5a6bd8
mhnazary <mhnazary2020@gmail.com>
2024-01-01 21:41:34 +0430

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Hello 4Learn-Website</h1>
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,25 @@
 function Navbar() {
   return (
     <>
-      <h1>Welcome to Navbar</h1>
+      <header className=" flex justify-between fixed py-4 px-12 bg-white w-full text-center shadow-sm">
+        <h1 className="text-3xl font-bold">4Learn-App</h1>
+        <div className="navbar w-9/12 flex justify-between text-center">
+          <div className="nav flex justify-evenly w-10/12 text-center text-lg pt-1">
+            <navLink>Home</navLink>
+            <navLink>About</navLink>
+            <navLink>Courses</navLink>
+            <navLink>Trainers</navLink>
+            <navLink>Events</navLink>
+            <navLink>Pricing</navLink>
+            <navLink>Dropdown</navLink>
+            <navLink>Contacts</navLink>
+          </div>
+          <button className="start bg-green-400 text-white px-5 rounded-3xl">
+            {" "}
+            Get started
+          </button>
+        </div>
+      </header>
     </>
   );
 }

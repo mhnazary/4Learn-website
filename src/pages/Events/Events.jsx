720ad016deeeb66119676255f7d1595204d2fe0c
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-02 10:36:13 +0430

    Add Events page with Navbar

---
+++ src/pages/Events/Events.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+function Events() {
+  return (
+    <>
+      <Navbar />
+      <h1>Welcome to Events</h1>
+    </>
+  );
+}
+
+export default Events;

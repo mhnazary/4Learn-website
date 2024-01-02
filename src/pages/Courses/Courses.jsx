2850ceb495f62b8c2b85f7c59df45c3d9025fcba
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-02 10:36:01 +0430

    Add Courses page with Navbar

---
+++ src/pages/Courses/Courses.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+function Courses() {
+  return (
+    <>
+      <Navbar />
+      <h1>Welcome to Courses</h1>
+    </>
+  );
+}
+
+export default Courses;

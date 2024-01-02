3c36fef96cb531017df8bc037acdd3a8ee614142
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-02 10:36:22 +0430

    Add Pricing page

---
+++ src/pages/Pricing/Pricing.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+function Pricing() {
+  return (
+    <>
+      <Navbar />
+      <h1>Welcome to Pricing</h1>
+    </>
+  );
+}
+
+export default Pricing;

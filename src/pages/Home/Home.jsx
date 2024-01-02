32cb7b02e740f133f66f073353c48b080713b023
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-02 10:36:17 +0430

    Add Home page component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import HomeHeader from "../../components/HomeHeader/HomeHeader";
+import Navbar from "../../components/Navbar/Navbar";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <HomeHeader />
+    </div>
+  );
+}
+
+export default Home;

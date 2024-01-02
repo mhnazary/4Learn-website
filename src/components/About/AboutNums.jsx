d488572953990978bf4d540603d369a212866be7
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-02 14:14:38 +0430

    Add AboutNums component

---
+++ src/components/About/AboutNums.jsx
@@ -0,0 +1,24 @@
+function AboutNums() {
+  return (
+    <div className="flex justify-around text-center items-center px-14 bg-[#f6f7f6] py-10">
+      <div className="">
+        <p className="text-[#5fcf80] font-bold text-5xl my-3">1232</p>
+        <p className="font-raleway font-600">Students</p>
+      </div>
+      <div>
+        <p className="text-[#5fcf80] font-bold text-5xl my-3">64</p>
+        <p className="font-raleway font-600">Courses</p>
+      </div>
+      <div>
+        <p className="text-[#5fcf80] font-bold text-5xl my-3">42</p>
+        <p className="font-raleway font-600">Events</p>
+      </div>
+      <div>
+        <p className="text-[#5fcf80] font-bold text-5xl my-3">15</p>
+        <p className="font-raleway font-600">Trainers</p>
+      </div>
+    </div>
+  );
+}
+
+export default AboutNums;

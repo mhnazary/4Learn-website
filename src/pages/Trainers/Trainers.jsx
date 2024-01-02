70b5d1a41bb4c5ce93ee9185b56d9fc90e1a8ee8
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-02 10:36:26 +0430

    Add Trainers page with Navbar

---
+++ src/pages/Trainers/Trainers.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+function Trainers() {
+  return (
+    <>
+      <Navbar />
+      <h1>Welcome to Trainers</h1>
+    </>
+  );
+}
+
+export default Trainers;

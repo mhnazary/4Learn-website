95184d1707a9a8561528215df13438da2e98b9f0
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-02 10:36:05 +0430

    Add Dropdown component

---
+++ src/pages/Dropdown/Dropdown.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+function Dropdown() {
+  return (
+    <>
+      <Navbar />
+      <h1>Welcome to Dropdown</h1>
+    </>
+  );
+}
+
+export default Dropdown;

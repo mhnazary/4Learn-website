2f6566b47eba32ea5aa9904d66b360b2c52ebefd
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-02 14:14:13 +0430

    Add components to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,20 @@
 import HomeHeader from "../../components/HomeHeader/HomeHeader";
+import HomeInfo from "../../components/About/About";
 import Navbar from "../../components/Navbar/Navbar";
+import AboutNums from "../../components/About/AboutNums";
+import HomeWhy from "../../components/HomeHeader/HomeWhy";
 
 function Home() {
   return (
     <div>
+  
       <Navbar />
+    <div className="mt-1">
       <HomeHeader />
+      <HomeInfo />
+      <AboutNums />
+      <HomeWhy />
+      </div>
     </div>
   );
 }

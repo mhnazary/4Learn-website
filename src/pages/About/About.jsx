83fe8a2d5509921d14edf0756daae5ea0663383a
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-02 10:35:52 +0430

    Add About page with Navbar

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+function About() {
+  return (
+    <>
+      <Navbar />
+      <h1>Welcome to About</h1>
+    </>
+  );
+}
+
+export default About;

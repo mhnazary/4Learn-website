5b6f5a002f9c91ebfd4f5f4fe2e051e1d75bb226
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-02 15:26:55 +0430

    Add Course and Courses components

---
+++ src/components/Home/Course.jsx
@@ -0,0 +1,29 @@
+function Course() {
+  return (
+        <div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <div>
+                    <p></p>
+                    <p></p>
+                </div>
+                <h4></h4>
+                <p></p>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                        <p></p>
+                    </div>
+                    <div>
+                        <p></p>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+  );
+}
+
+export default Course;

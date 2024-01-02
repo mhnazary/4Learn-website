3bc997e9492e8fffa7a59b336fee516a26455110
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-02 10:35:57 +0430

    Add Contact page component

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar/Navbar";
+
+function Contacts() {
+  return (
+    <>
+      <Navbar />
+      <h1>Welcome to Contacts</h1>
+    </>
+  );
+}
+
+export default Contacts;

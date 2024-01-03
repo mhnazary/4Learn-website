12ff91717032540ff67c6ace353d241042527bc6
mhnazary <mhnazary2020@gmail.com>
2024-01-03 16:31:25 +0430

    Refactor The slider

--- src/components/About/Slider.jsx
+++ src/components/About/Slider.jsx
@@ -1,113 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import SliderCard from "./SliderCard";
 
+const information = [
+  {
+    name: "Hafizullah",
+    job: "Ceo and Founder",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Consequuntur esse quaerat explicabo quo iure voluptas dolorem fugit, ipsam accusamus modi.",
+  },
+  {
+    name: "Mohammad Asif",
+    job: "WHO Conrdinator",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Consequuntur esse quaerat explicabo quo iure voluptas dolorem fugit, ipsam accusamus modi.",
+  },
+  {
+    name: "Reza",
+    job: "Pubg Pro player",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Consequuntur esse quaerat explicabo quo iure voluptas dolorem fugit, ipsam accusamus modi.",
+  },
+  {
+    name: "Mohammad anwar",
+    job: "Visa registration Especialist",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Consequuntur esse quaerat explicabo quo iure voluptas dolorem fugit, ipsam accusamus modi.",
+  },
+  {
+    name: "Mohammad Zahir",
+    job: "Dari poet",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Consequuntur esse quaerat explicabo quo iure voluptas dolorem fugit, ipsam accusamus modi.",
+  },
+  {
+    name: "Khan Mohammad",
+    job: "Professor",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Consequuntur esse quaerat explicabo quo iure voluptas dolorem fugit, ipsam accusamus modi.",
+  },
+];
 const MultipleItems = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const handleSlideChange = (index) => {
+    setCurrentSlide(index);
+  };
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 300,
     slidesToShow: 2,
     slidesToScroll: 2,
+    customPaging: (i) => {
+      const dotClassName = i === currentSlide ? "dot active" : "dot";
+      return <div className={dotClassName} />;
+    },
   };
 
   return (
     <div className=" py-16 px-10">
+      <div className="flex justify-start items-center">
+        <h2 className="text-[14px] font-raleway font-bold text-[#aaaaaa] tracking-widest">
+          TESTIMONIALS
+        </h2>
+        <hr className="w-32 ml-4 border-1 border-green" />
+      </div>
       <h1 className="text-[36px] font-600 font-poppins mb-16">
         WHAT ARE THEY SAYING
       </h1>
-      <Slider {...settings}>
-        <div className=" w-4/6 h-60 px-14 mb-8">
-          <div className="border-2 border-gray-200 px-14 py-10 mt-4 rounded-lg">
-            <h1 className="font-raleway text-[18px] font-bold">
-              Hafizullah Rasa
-            </h1>
-            <p className="text-[14px] font-sans text-[#999999] pb-3">
-              Ceo and Founder
-            </p>
-            <span className="text-[16px] font-sans text-[#444444] italic">
-              <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Consequuntur esse quaerat explicabo quo iure voluptas dolorem
-              fugit, ipsam accusamus modi.
-              <i className="bx bxs-quote-alt-left quote-icon-left"></i>
-            </span>
+      <Slider {...settings} afterChange={handleSlideChange}>
+        {information.map((info) => (
+          <div>
+            {" "}
+            <SliderCard Information={info} />{" "}
           </div>
-        </div>
-        <div className=" w-4/6 h-60 px-14 mb-8">
-          <div className="border-2 border-gray-200 px-14 py-10 mt-4 rounded-lg">
-            <h1 className="font-raleway text-[18px] font-bold">
-              Mohammad Asif
-            </h1>
-            <p className="text-[14px] font-sans text-[#999999] pb-3">
-              WHO Cordinator
-            </p>
-            <span className="text-[16px] font-sans text-[#444444] italic">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Consequuntur esse quaerat explicabo quo iure voluptas dolorem
-              fugit, ipsam accusamus modi.
-            </span>
-          </div>
-        </div>
-        <div className=" w-4/6 h-60 px-14 mb-8">
-          <div className="border-2 border-gray-200 px-14 py-10 mt-4 rounded-lg">
-            <h1 className="font-raleway text-[18px] font-bold">Reza Merzai</h1>
-            <p className="text-[14px] font-sans text-[#999999] pb-3">
-              Pubg Player Pro
-            </p>
-            <span className="text-[16px] font-sans text-[#444444] italic">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Consequuntur esse quaerat explicabo quo iure voluptas dolorem
-              fugit, ipsam accusamus modi.
-            </span>
-          </div>
-        </div>
-        <div className=" w-4/6 h-60 px-14 mb-8">
-          <div className="border-2 border-gray-200 px-14 py-10 mt-4 rounded-lg">
-            <h1 className="font-raleway text-[18px] font-bold">
-              Mohammad Anwar
-            </h1>
-            <p className="text-[14px] font-sans text-[#999999] pb-3">
-              Visa Registration Specialist
-            </p>
-            <span className="text-[16px] font-sans text-[#444444] italic">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Consequuntur esse quaerat explicabo quo iure voluptas dolorem
-              fugit, ipsam accusamus modi.
-            </span>
-          </div>
-        </div>
-        <div className=" w-4/6 h-60 px-14 mb-8">
-          <div className="border-2 border-gray-200 px-14 py-10 mt-4 rounded-lg">
-            <h1 className="font-raleway text-[18px] font-bold">
-              Mohammad Zahir
-            </h1>
-            <p className="text-[14px] font-sans text-[#999999] pb-3">
-              Dari Poet
-            </p>
-            <span className="text-[16px] font-sans text-[#444444] italic">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Consequuntur esse quaerat explicabo quo iure voluptas dolorem
-              fugit, ipsam accusamus modi.
-            </span>
-          </div>
-        </div>
-        <div className=" w-4/6 h-60 px-14 mb-8">
-          <div className="border-2 border-gray-200 px-14 py-10 mt-4 rounded-lg">
-            <h1 className="font-raleway text-[18px] font-bold">
-              Khan Mohammad
-            </h1>
-            <p className="text-[14px] font-sans text-[#999999] pb-3">
-              Professor
-            </p>
-            <span className="text-[16px] font-sans text-[#444444] italic">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Consequuntur esse quaerat explicabo quo iure voluptas dolorem
-              fugit, ipsam accusamus modi.
-            </span>
-          </div>
-        </div>
+        ))}
       </Slider>
     </div>
   );

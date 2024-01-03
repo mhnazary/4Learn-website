b6d9e8c3000f0c4e1b3d42388ce78b8adb7e0daa
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-03 11:53:07 +0430

    Add EventsHeader component

---
+++ src/components/Events/EventsHeader.jsx
@@ -0,0 +1,12 @@
+import Header from "../Courses/Header";
+
+function Events() {
+  return (
+    <Header
+        title="Events"
+        text=" Est dolorum ut non facere possimus quibusdam eligendi voluptatem. Quia id aut similique quia voluptas sit quaerat debitis. Rerum omnis ipsam aperiam consequatur laboriosam nemo harum praesentium."
+      />
+  );
+}
+
+export default Events;

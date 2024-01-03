931ac4ec887a3707037c10daf5879b2ba4888b14
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-03 11:52:50 +0430

    Add CoursesHeader component

---
+++ src/components/Courses/CoursesHeader.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+
+function CoursesHeader() {
+  return (
+    <div>
+      <Header
+        title="Courses"
+        text=" Est dolorum ut non facere possimus quibusdam eligendi voluptatem. Quia
+        id aut similique quia voluptas sit quaerat debitis. Rerum omnis ipsam
+        aperiam consequatur laboriosam nemo harum praesentium."
+      />
+    </div>
+  );
+}
+
+export default CoursesHeader;

79eb79eb7af3bc7666fac6c87eab129236b41b29
hafiz1379 <hafizrasa1379@gmail.com>
2024-01-03 11:52:57 +0430

    Add CoursesHeader, CouresesCards, and Footer components to Courses page

--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -1,11 +1,20 @@
 import Navbar from "../../components/Navbar/Navbar";
+import CoursesHeader from "../../components/Courses/CoursesHeader";
+import CouresesCards from "../../components/Home/CoursesCards";
+import Footer from "../../components/Footer/Footer";
 
 function Courses() {
   return (
-    <>
+    <div>
       <Navbar />
-      <h1>Welcome to Courses</h1>
-    </>
+      <div className="mt-16">
+          <CoursesHeader />
+        <div className="space-y-14 mt-40">
+        <CouresesCards />
+        <Footer />
+        </div>
+      </div>
+    </div>
   );
 }
 

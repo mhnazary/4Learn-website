8694f3f2d4e91e2cf564c482e752f59ef630e58e
mhnazary <mhnazary2020@gmail.com>
2024-01-04 10:18:05 +0430

    Added Contact part to Contact page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,14 +1,14 @@
 import HomeInfo from "../../components/About/About";
 import AboutNums from "../../components/About/AboutNums";
 import Navbar from "../../components/Navbar/Navbar";
-import MultipleItems from '../../components/About/Slider.jsx'
+import MultipleItems from "../../components/About/Slider.jsx";
 
 function About() {
   return (
     <>
       <Navbar />
       <div className="pt-16">
-        <AboutPart />
+        <AboutPart part="About Us" />
         <HomeInfo />
         <AboutNums />
         <MultipleItems />
@@ -17,11 +17,11 @@ function About() {
   );
 }
 
-function AboutPart() {
+function AboutPart({ part }) {
   return (
     <>
-      <div className="px-4 lg:px-10 bg-green flex flex-col justify-center items-center py-10 text-white ">
-        <h1 className="text-[32px] font-raleway">About Us</h1>
+      <div className="px-4 lg:px-10 bg-green flex flex-col justify-center items-center py-8 text-white ">
+        <h1 className="text-[32px] font-raleway font-bold">{part}</h1>
         <p className="text-center md:text-center text-[14px] font-sans">
           Est dolorum ut non facere possimus quibusdam eligendi voluptatem. Quia
           id aut similique quia voluptas sit quaerat debitis. Rerum omnis ipsam
@@ -32,4 +32,4 @@ function AboutPart() {
   );
 }
 
-export default About;
+export { About, AboutPart };
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,13 @@
 import Navbar from "../../components/Navbar/Navbar";
+import {AboutPart} from "../About/About";
 
 function Contacts() {
   return (
     <>
-      <Navbar />
-      <h1>Welcome to Contacts</h1>
+      <Navbar />{" "}
+      <div className="pt-16">
+        <AboutPart part="Contact Us" />
+      </div>
     </>
   );
 }
